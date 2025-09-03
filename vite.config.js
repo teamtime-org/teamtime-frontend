d8876246157d06d4950b2b7083c64ad0cfc48d90
Francisco Salinas <fsalinas@outlook.com>
2025-09-02 23:00:20 -0600

    feat: Reintroduce GitHub Actions workflow for deploying frontend to GitHub Pages and update base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/teamtime/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/teamtime-frontend/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
