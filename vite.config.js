53de01d211c84d12b9036cf218c041c709cab215
Francisco Salinas <fsalinas@outlook.com>
2025-09-22 14:35:35 -0600

    fix: update base URL in vite.config.js to use the correct path for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/',
+  base: process.env.VITE_BASE_URL || '/teamtime-frontend/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
